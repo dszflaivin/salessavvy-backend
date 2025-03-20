2e2eba74f5b7aa658ff94387aea75605be50ba64
dszflaivin <20cs051.flaivin@sjec.ac.in>
2025-03-20 17:39:53 +0530

    Removed dotenv

--- src/main/java/com/example/demo/SalesSavvyApplication.java
+++ src/main/java/com/example/demo/SalesSavvyApplication.java
@@ -1,23 +1,11 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class SalesSavvyApplication {
-
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-	    System.setProperty("DB_URL", dotenv.get("DB_URL"));
-	    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-	    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-	    System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-	    System.setProperty("RAZORPAY_KEY_ID", dotenv.get("RAZORPAY_KEY_ID"));
-	    System.setProperty("RAZORPAY_KEY_SECRET", dotenv.get("RAZORPAY_KEY_SECRET"));
-	    System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-
 		SpringApplication.run(SalesSavvyApplication.class, args);
 	}
 }
