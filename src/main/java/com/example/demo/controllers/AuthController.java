ff091200c62da6d4c3cbd39d5f7bbc6b5efe36d2
dszflaivin <20cs051.flaivin@sjec.ac.in>
2025-03-22 13:02:16 +0530

    setDomain

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -45,7 +45,7 @@ public class AuthController {
 			cookie.setSecure(false);
 			cookie.setPath("/");
 			cookie.setMaxAge(3600);
-			cookie.setDomain("localhost");
+			cookie.setDomain("salessavvy-backend-z6no.onrender.com");
 			response.addCookie(cookie);
 			
 			response.addHeader("Set-Cookie",
