e1c72045a2a35df44cda7f1ce80c3cd0dbdbd15b
dszflaivin <106856746+dszflaivin@users.noreply.github.com>
2025-03-24 15:44:18 +0530

    Update AuthController.java

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -45,7 +45,7 @@ public class AuthController {
 			cookie.setSecure(false);
 			cookie.setPath("/");
 			cookie.setMaxAge(3600);
-			cookie.setDomain(null);
+			cookie.setDomain("localhost");
 			response.addCookie(cookie);
 			
 			response.addHeader("Set-Cookie",
